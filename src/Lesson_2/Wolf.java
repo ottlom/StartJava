b70ee2ad8ff48d46433942233a64ead4b8c6a46b
Maksim Lapushkin <ottlom@mail.ru>
2022-08-27 04:15:15 +0300

    Solve Wolf task with getters and setters

--- src/Lesson_2/Wolf.java
+++ src/Lesson_2/Wolf.java
@@ -1,11 +1,55 @@
 package Lesson_2;
 
 public class Wolf {
-    char gender;
-    String name;
-    float weight;
-    int age;
-    String color;
+    private char gender;
+    private String name;
+    private float weight;
+    private int age;
+    private String color;
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+       if (age > 8) {
+           System.out.println("Некорректный возраст");
+       } else {
+           this.age = age;
+       }
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 
     public void walk() {
         System.out.println("wolf " + name + " walk");
--- src/Lesson_2/WolfTest.java
+++ src/Lesson_2/WolfTest.java
@@ -3,13 +3,13 @@ package Lesson_2;
 public class WolfTest {
     public static void main(String[] args) {
         Wolf wolf = new Wolf();
-        wolf.gender = 'M';
-        wolf.name = "Fenrir";
-        wolf.weight = 25.7f;
-        wolf.age = 10;
-        wolf.color = "Gray";
-        System.out.println("wolf name is " + wolf.name + " his parameters: " + "gender = " + wolf.gender +
-                ", weight = " + wolf.weight + ", age = " + wolf.age + ", color = " + wolf.color);
+        wolf.setGender('M');
+        wolf.setName("Fenrir");
+        wolf.setWeight(25.7f);
+        wolf.setAge(6);
+        wolf.setColor("Gray");
+        System.out.println("wolf name is " + wolf.getName() + " his parameters: " + "gender = " + wolf.getGender() +
+                ", weight = " + wolf.getWeight() + ", age = " + wolf.getAge() + ", color = " + wolf.getColor());
         wolf.walk();
         wolf.howl();
         wolf.hunt();
