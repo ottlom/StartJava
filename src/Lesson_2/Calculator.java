94a1e1dd0202f58b4e82575696f2324e0b52bed7
Maksim Lapushkin <ottlom@mail.ru>
2022-09-02 13:33:03 +0300

    delete unnecessary import from Calculator class

--- src/Lesson_2/Calculator.java
+++ src/Lesson_2/Calculator.java
@@ -1,41 +1,58 @@
 package Lesson_2;
 
-import java.util.Scanner;
-
 public class Calculator {
-    int num1;
-    int num2;
-    char mathSign;
-    int result;
-    Scanner scan;
-    boolean alive = true;
-    String nextCountOrNot;
+    private int num1;
+    private int num2;
+    private char mathSign;
+
+    public void setNum1(int num1) {
+        this.num1 = num1;
+    }
+
+    public int getNum1() {
+        return num1;
+    }
+
+    public void setNum2(int num2) {
+        this.num2 = num2;
+    }
+
+    public int getNum2() {
+        return num2;
+    }
+
+    public void setMathSign(char mathSign) {
+        this.mathSign = mathSign;
+    }
+
+    public char getMathSign() {
+        return mathSign;
+    }
 
     public void calculate() {
         switch (mathSign) {
             case '+':
-                result = num1 + num2;
+                System.out.println(num1 + num2);
                 break;
             case '-':
-                result = num1 - num2;
+                System.out.println(num1 - num2);
                 break;
             case '*':
-                result = num1 * num2;
+                System.out.println(num1 * num2);
                 break;
             case '/':
-                result = num1 / num2;
+                System.out.println(num1 / num2);
                 break;
             case '^':
-                result = num1;
                 for (int i = num2; i > 1; i--) {
-                    result *= num1;
+                    num1 *= num1;
                 }
+                System.out.println(num1);
                 break;
             case '%':
-                result = num1 % num2;
+                System.out.println(num1 % num2);
                 break;
         }
-        System.out.println(result);
     }
 }
 
--- src/Lesson_2/CalculatorTest.java
+++ src/Lesson_2/CalculatorTest.java
@@ -3,30 +3,35 @@ package Lesson_2;
 import java.util.Scanner;
 
 public class CalculatorTest {
+
     public static void main(String[] args) {
+        boolean alive = true;
         Calculator calculator = new Calculator();
-        calculator.scan = new Scanner(System.in);
-        while (calculator.alive) {
+        Scanner scan = new Scanner(System.in);
+        String nextCountOrNot;
+
+        while (alive) {
             System.out.print("Введите первое число: ");
-            calculator.num1 = calculator.scan.nextInt();
-            calculator.scan.nextLine();
+            calculator.setNum1(scan.nextInt());
+            calculator.getNum1();
             System.out.print("Введите знак математической операции: ");
-            calculator.mathSign = calculator.scan.nextLine().charAt(0);
+            scan.nextLine();
+            calculator.setMathSign(scan.nextLine().charAt(0));
+            calculator.getMathSign();
             System.out.print("Введите второе число: ");
-            calculator.num2 = calculator.scan.nextInt();
+            calculator.setNum2(scan.nextInt());
+            calculator.getNum2();
             calculator.calculate();
             System.out.println("Хотите продолжить вычисления?(yes/no)");
-            calculator.scan.nextLine();
-            calculator.nextCountOrNot = calculator.scan.nextLine();
-            if (!calculator.nextCountOrNot.equals("yes") & !calculator.nextCountOrNot.equals("no")) {
-                while (!calculator.nextCountOrNot.equals("yes") & !calculator.nextCountOrNot.equals("no")) {
-                    System.out.println("Хотите продолжить вычисления?(yes/no)");
-                    calculator.nextCountOrNot = calculator.scan.nextLine();
-                }
+            scan.nextLine();
+            nextCountOrNot = scan.nextLine();
+            while (!nextCountOrNot.equals("yes") && !nextCountOrNot.equals("no")) {
+                System.out.println("Хотите продолжить вычисления?(yes/no)");
+                nextCountOrNot = scan.nextLine();
+                if (nextCountOrNot.equals("no")) {
+                    alive = false;
+                } else if (nextCountOrNot.equals("yes"));
             }
-            if (calculator.nextCountOrNot.equals("no")) {
-                calculator.alive = false;
-            } else if (calculator.nextCountOrNot.equals("yes"));
         }
     }
 }
