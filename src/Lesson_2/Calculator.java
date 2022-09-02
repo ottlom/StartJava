a70d92a7048f7b3c6e26baec079a2e8227156075
Maksim Lapushkin <ottlom@mail.ru>
2022-09-02 18:34:18 +0300

    correct remarks

--- src/Lesson_2/Calculator.java
+++ src/Lesson_2/Calculator.java
@@ -9,50 +9,38 @@ public class Calculator {
         this.num1 = num1;
     }
 
-    public int getNum1() {
-        return num1;
-    }
-
     public void setNum2(int num2) {
         this.num2 = num2;
-    }
-
-    public int getNum2() {
-        return num2;
     }
 
     public void setMathSign(char mathSign) {
         this.mathSign = mathSign;
     }
 
-    public char getMathSign() {
-        return mathSign;
-    }
-
     public void calculate() {
         switch (mathSign) {
             case '+':
-                System.out.println(num1 + num2);
+                num1 += num2;
                 break;
             case '-':
-                System.out.println(num1 - num2);
+                num1 -= num2;
                 break;
             case '*':
-                System.out.println(num1 * num2);
+                num1 *= num2;
                 break;
             case '/':
-                System.out.println(num1 / num2);
+                num1 /= num2;
                 break;
             case '^':
                 for (int i = num2; i > 1; i--) {
                     num1 *= num1;
                 }
-                System.out.println(num1);
                 break;
             case '%':
-                System.out.println(num1 % num2);
+                num1 %= num2;
                 break;
         }
+        System.out.println(num1);
     }
 }
 
--- src/Lesson_2/CalculatorTest.java
+++ src/Lesson_2/CalculatorTest.java
@@ -5,32 +5,26 @@ import java.util.Scanner;
 public class CalculatorTest {
 
     public static void main(String[] args) {
-        boolean alive = true;
         Calculator calculator = new Calculator();
         Scanner scan = new Scanner(System.in);
-        String nextCountOrNot;
+        String option = "yes";
 
-        while (alive) {
+        while (option.equals("yes")) {
             System.out.print("Введите первое число: ");
             calculator.setNum1(scan.nextInt());
-            calculator.getNum1();
             System.out.print("Введите знак математической операции: ");
             scan.nextLine();
             calculator.setMathSign(scan.nextLine().charAt(0));
-            calculator.getMathSign();
             System.out.print("Введите второе число: ");
             calculator.setNum2(scan.nextInt());
-            calculator.getNum2();
             calculator.calculate();
-            System.out.println("Хотите продолжить вычисления?(yes/no)");
-            scan.nextLine();
-            nextCountOrNot = scan.nextLine();
-            while (!nextCountOrNot.equals("yes") && !nextCountOrNot.equals("no")) {
+            option = scan.nextLine();
+            while (!option.equals("yes") && !option.equals("no")) {
                 System.out.println("Хотите продолжить вычисления?(yes/no)");
-                nextCountOrNot = scan.nextLine();
-                if (nextCountOrNot.equals("no")) {
-                    alive = false;
-                } else if (nextCountOrNot.equals("yes"));
+                option = scan.nextLine();
+                if (option.equals("no")) {
+                    break;
+                }
             }
         }
     }
