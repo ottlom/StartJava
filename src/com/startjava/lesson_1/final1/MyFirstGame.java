3c3d11229c2511188ddf7fd7f375bed883ebd1e3
Maksim Lapushkin <ottlom@mail.ru>
2022-09-18 21:55:19 +0300

    change structure of packages and files

---
+++ src/com/startjava/lesson_1/final1/MyFirstGame.java
@@ -0,0 +1,20 @@
+package com.startjava.lesson_1.final1;
+
+public class MyFirstGame {
+    public static void main(String[] args) {
+        int numComp = 16;
+        int numPlayer = 67;
+        while (numPlayer != numComp) {
+            if (numPlayer < numComp) {
+                System.out.println("число " + numPlayer + " меньше того, что загадал компьютер");
+                numPlayer += 3;
+            } else if (numPlayer > numComp) {
+                System.out.println("число " + numPlayer + " больше того, что загадал компьютер");
+                numPlayer -= 4;
+            }
+            if (numPlayer == numComp) {
+                System.out.println("Вы победили!");
+            }
+        }
+    }
+}
---
+++ src/com/startjava/lesson_2_3/animal/Wolf.java
@@ -0,0 +1,73 @@
+package com.startjava.lesson_2_3.animal;
+
+public class Wolf {
+    private char gender;
+    private String name;
+    private float weight;
+    private int age;
+    private String color;
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+       if (age > 8) {
+           System.out.println("Некорректный возраст");
+       } else {
+           this.age = age;
+       }
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void walk() {
+        System.out.println("wolf " + name + " walk");
+    }
+
+    public void sit() {
+        System.out.println("wolf " + name +" sit");
+    }
+
+    public void run() {
+        System.out.println("wolf " + name +" run");
+    }
+
+    public void howl() {
+        System.out.println("wolf " + name + " howl");
+    }
+
+    public void hunt() {
+        System.out.println("wolf " + name + " hunt");
+    }
+}
